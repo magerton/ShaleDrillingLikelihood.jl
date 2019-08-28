5a9a0c2c3d38c65c60c401f2f02839749ad87926
Mark <magerton@rice.edu>
2019-08-28 09:28:58 -0700

    broke out LLi vs LL

--- test/drilling.jl
+++ test/drilling.jl
@@ -53,32 +53,47 @@ drng = 1:L
 M = 100
 psisim = randn(M,num_i)
 
-function flow(d::Integer,theta::AbstractVector,x::Real,ψ::Real)
+function flow(d::Integer,theta::AbstractVector,x::Real,ψ::Real,L::Integer)
     @assert 1 <= d <= L  # FIXME with L
     d==1 && return zero(x)
     return theta[d-1]*x + theta[d+1]*ψ
 end
 
+function loglik_i(yi::AbstractVector{Int}, xi::AbstractVector{<:Real}, psii::AbstractVector{<:Real}, theta::AbstractVector{<:Real}, ubv::AbstractVector{<:Real}, llm::AbstractVector{T}, L::Integer) where {T}
+    M = length(psii)
+    L = length(ubv)
+    num_t = length(yi)
+    @assert num_t == length(xi)
+    @assert M == length(llm)
 
-function loglik(y, x, psi, theta, ubv, llm, num_t)
-    M, num_i = size(psi)
-    @assert (1,3,) == extrema(y)
-    @assert length(ubv) == 3
-    length(y) == length(x) == num_i * num_t
+    fill!(llm, zero(T))
 
-    LL = 0.0
-    for i in 1:num_i
-        fill!(llm, 0.0)
+    for t in 1:num_t
         for m in 1:M
-            for t in 1:num_t
-                n = (i-1)*num_t + t
-                @inbounds @simd for d in 1:3
-                    ubv[d] = flow(d,theta,x[n],psi[m,i])
-                end
-                llm[m] += ubv[y[n]] - logsumexp(ubv)
+            @inbounds @simd for d in 1:L
+                ubv[d] = flow(d, theta, xi[t], psii[m], L)
             end
-            LL += logsumexp(llm)
+            d_choice = yi[t]
+            llm[m] += ubv[d_choice] - logsumexp(ubv)
         end
+    end
+    return logsumexp(llm)
+end
+
+
+function loglik(y, x, psi, theta::AbstractVector{T}, ubv, llm, num_t::Integer, num_i::Integer) where {T}
+    @assert length(y) == length(x) == num_i * num_t
+    L = maximum(y)
+    @assert length(ubv) == L
+    @assert minimum(y) == 1
+    M = size(psi,1)
+    @assert size(psi,2) == num_i
+
+    LL = zero(T)
+
+    for i in 1:num_i
+        irng = ((i-1)*num_t) .+ 1:num_t
+        @views LL += loglik_i(y[irng], x[irng], psi[:,i], theta, ubv, llm, L)
     end
     return LL
 end
@@ -86,9 +101,9 @@ end
 ubvtmp = Vector{Float64}(undef, L)
 llmtmp = Vector{Float64}(undef, M)
 
-f(θ) = -loglik(choices, X, psisim, θ, ubvtmp, llmtmp, num_t)
+f(θ) = -loglik(choices, X, psisim, θ, ubvtmp, llmtmp, num_t, num_i)
 
-res = optimize(f, theta, NelderMead(), Optim.Options(time_limit=100.0, show_trace=true))
+res = optimize(f, theta, BFGS(), Optim.Options(time_limit=60.0*5, show_trace=true))
 
 @show res
 @show res.minimizer
