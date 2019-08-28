36467129d0068e09b37ae97f8d6c9b7ae1b3a777
Mark <magerton@rice.edu>
2019-08-28 10:25:52 -0700

    work on testing threaded parallelization
    we get on a 40% speedup with 8 cores. why?

--- test/drilling.jl
+++ test/drilling.jl
@@ -8,7 +8,9 @@ using Distributions
 using Optim
 using Random
 using LinearAlgebra
-#
+using BenchmarkTools
+using Base.Threads
+
 # @testset "Drilling Likelihood" begin
 
 
@@ -21,8 +23,8 @@ using LinearAlgebra
 
 Random.seed!(1234)
 
-num_i = 1_000
-num_t = 25
+num_i = 10_000
+num_t = 500
 nobs = num_i *num_t
 β = [1.0, -2.0, 1.0, 2.0]
 k = length(β)
@@ -59,9 +61,9 @@ function flow(d::Integer,theta::AbstractVector,x::Real,ψ::Real,L::Integer)
     return theta[d-1]*x + theta[d+1]*ψ
 end
 
-function loglik_i(yi::AbstractVector{Int}, xi::AbstractVector{<:Real}, psii::AbstractVector{<:Real}, theta::AbstractVector{<:Real}, ubv::AbstractVector{<:Real}, llm::AbstractVector{T}, L::Integer) where {T}
+function loglik_i(yi::AbstractVector{Int}, xi::AbstractVector{<:Real}, psii::AbstractVector{<:Real}, theta::AbstractVector{<:Real}, ubv::AbstractArray{<:Real}, llm::AbstractVector{T}, L::Integer) where {T}
     M = length(psii)
-    L = length(ubv)
+    L = size(ubv,1)
     num_t = length(yi)
     @assert num_t == length(xi)
     @assert M == length(llm)
@@ -70,38 +72,49 @@ function loglik_i(yi::AbstractVector{Int}, xi::AbstractVector{<:Real}, psii::Abs
 
     for t in 1:num_t
         for m in 1:M
-            @inbounds @simd for d in 1:L
-                ubv[d] = flow(d, theta, xi[t], psii[m], L)
+            @simd for d in 1:L
+                @inbounds ubv[d] = flow(d, theta, xi[t], psii[m], L)
             end
             d_choice = yi[t]
-            llm[m] += ubv[d_choice] - logsumexp(ubv)
+            @views llm[m] += ubv[d_choice] - logsumexp(ubv)
         end
     end
     return logsumexp(llm)
 end
 
 
-function loglik(y, x, psi, theta::AbstractVector{T}, ubv, llm, num_t::Integer, num_i::Integer) where {T}
+irng(num_t::Int,i::Int) = ((i-1)*num_t) .+ 1:num_t
+
+function loglik(y::AbstractVector, x::AbstractArray, psi::AbstractArray, theta::AbstractVector{T}, ubv::AbstractArray, llm::AbstractArray, num_t::Integer, num_i::Integer) where {T}
     @assert length(y) == length(x) == num_i * num_t
     L = maximum(y)
-    @assert length(ubv) == L
-    @assert minimum(y) == 1
     M = size(psi,1)
+    @assert size(ubv) == (L,nthreads())
+    @assert minimum(y) == 1
     @assert size(psi,2) == num_i
 
-    LL = zero(T)
-
+    LL = Atomic{T}(zero(T))
     for i in 1:num_i
-        irng = ((i-1)*num_t) .+ 1:num_t
-        @views LL += loglik_i(y[irng], x[irng], psi[:,i], theta, ubv, llm, L)
+        rng = irng(num_t,i)
+        @views atomic_add!(LL, loglik_i(y[rng], x[rng], psi[:,i], theta, ubv[:,threadid()], llm[:,threadid()], L))
     end
-    return LL
+    return LL[]
 end
 
-ubvtmp = Vector{Float64}(undef, L)
-llmtmp = Vector{Float64}(undef, M)
+ubvtmp = Array{Float64}(undef, L, nthreads())
+llmtmp = Array{Float64}(undef, M, nthreads())
 
 f(θ) = -loglik(choices, X, psisim, θ, ubvtmp, llmtmp, num_t, num_i)
+@btime f(theta)
+
+
+
+@code_warntype loglik(choices, X, psisim, theta, ubvtmp, llmtmp, num_t, num_i)
+let y = choices[irng(num_t,1)], x = X[irng(num_t,1)], psii = psisim[:,1], ubv = ubvtmp[:,1], llm = llmtmp[:,1]
+    @code_warntype loglik_i(y, x, psii, theta, ubv, llm, L)
+end
+
+
 
 res = optimize(f, theta, BFGS(), Optim.Options(time_limit=60.0*5, show_trace=true))
 
