9fa1d85cbdeab3b59b009f7da64762f7240f6e4f
Mark <magerton@rice.edu>
2019-08-27 22:00:39 -0700

    made test dgp and SML that converges under nelder mead

--- test/drilling.jl
+++ test/drilling.jl
@@ -5,7 +5,7 @@
 using StatsFuns
 using Distributions
 # using Calculus
-# using Optim
+using Optim
 using Random
 using LinearAlgebra
 #
@@ -21,15 +21,19 @@ using LinearAlgebra
 
 Random.seed!(1234)
 
-nobs = 10
-β = [0.0, 1.0, -2.0]
+num_i = 1_000
+num_t = 25
+nobs = num_i *num_t
+β = [1.0, -2.0, 1.0, 2.0]
 k = length(β)
+L = 3
 
 # exogenous variable
 X = randn(nobs)
+psi = repeat(randn(num_i), inner=num_t)
 
 # choice utilities
-u = β*X'
+u = vcat(zeros(nobs)', β[1:2]*X' .+ β[3:4]*psi')
 
 # multinomial logit probabilities
 Pr0 = mapslices(softmax, u; dims=1)
@@ -43,17 +47,51 @@ choices = [searchsortedfirst(view(cum_Pr0, :, i), e_quantile[i]) for i in 1:leng
 # form likelihood
 # -----------------------------------------------
 
-
 theta = rand(k)
-ubV = zeros(k, nobs)
-mul!(ubV, theta, X')
+ubV = zeros(L, nobs)
+drng = 1:L
+M = 100
+psisim = randn(M,num_i)
 
-
-@inbounds @simd for d in drng
-    ubv[d+1] = flow(u,d,obs) + β * dynamic
+function flow(d::Integer,theta::AbstractVector,x::Real,ψ::Real)
+    @assert 1 <= d <= L  # FIXME with L
+    d==1 && return zero(x)
+    return theta[d-1]*x + theta[d+1]*ψ
 end
 
 
+function loglik(y, x, psi, theta, ubv, llm, num_t)
+    M, num_i = size(psi)
+    @assert (1,3,) == extrema(y)
+    @assert length(ubv) == 3
+    length(y) == length(x) == num_i * num_t
+
+    LL = 0.0
+    for i in 1:num_i
+        fill!(llm, 0.0)
+        for m in 1:M
+            for t in 1:num_t
+                n = (i-1)*num_t + t
+                @inbounds @simd for d in 1:3
+                    ubv[d] = flow(d,theta,x[n],psi[m,i])
+                end
+                llm[m] += ubv[y[n]] - logsumexp(ubv)
+            end
+            LL += logsumexp(llm)
+        end
+    end
+    return LL
+end
+
+ubvtmp = Vector{Float64}(undef, L)
+llmtmp = Vector{Float64}(undef, M)
+
+f(θ) = -loglik(choices, X, psisim, θ, ubvtmp, llmtmp, num_t)
+
+res = optimize(f, theta, NelderMead(), Optim.Options(time_limit=100.0, show_trace=true))
+
+@show res
+@show res.minimizer
 
 # end
 #
