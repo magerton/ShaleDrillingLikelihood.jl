2380a4c1154e5b8a37f9ae72d6fcdbf36e972a34
Mark <magerton@rice.edu>
2019-10-15 19:09:35 -0700

    un-nest some of testsets

--- test/data/drilling.jl
+++ test/data/drilling.jl
@@ -28,6 +28,12 @@ using ShaleDrillingLikelihood: SimulationDraws, _u, _v, SimulationDrawsMatrix, S
     uniti
 
 
+function randsumtoone(n)
+    x = rand(n)
+    x ./= sum(x)
+    return x
+end
+
 @testset "Drilling Data Structure" begin
 
     @testset "Exog Time Vars" begin
@@ -69,7 +75,6 @@ using ShaleDrillingLikelihood: SimulationDraws, _u, _v, SimulationDrawsMatrix, S
     end
 
 
-
     @testset "Iteration over DataDrill" begin
         nt = 12
         _j1ptr   = [1, 2]
@@ -85,64 +90,63 @@ using ShaleDrillingLikelihood: SimulationDraws, _u, _v, SimulationDrawsMatrix, S
 
         data = DataDrill(DrillModel(), _j1ptr, _j2ptr, _tptr, _jtstart, _jchars, _ichars, y, x, _zchars)
 
-        @testset "DataDrill" begin
-            @test length(data) == 1
-            @test maxj1length(data) == 1
+        @test length(data) == 1
+        @test maxj1length(data) == 1
 
-            @test j1ptr(data, 1) == 1
-            @test j1_range(data,1) == 1:1
-            @test j1length(data,1) == 1
-            @test j1stop(data,1) == 1
-            @test hasj1ptr(data) == true
+        @test j1ptr(data, 1) == 1
+        @test j1_range(data,1) == 1:1
+        @test j1length(data,1) == 1
+        @test j1stop(data,1) == 1
+        @test hasj1ptr(data) == true
 
-            @test tstart(data,1) == 1
-            @test tstop(data,1) == 4
-            @test trange(data,1) == 1:4
-            @test tlength(data,1) == 4
+        @test tstart(data,1) == 1
+        @test tstop(data,1) == 4
+        @test trange(data,1) == 1:4
+        @test tlength(data,1) == 4
 
-            @test tstart(data,2) == 5
-            @test tstop(data,2) == 5
-            @test trange(data,2) == 5:5
-            @test tlength(data,2) == 1
+        @test tstart(data,2) == 5
+        @test tstop(data,2) == 5
+        @test trange(data,2) == 5:5
+        @test tlength(data,2) == 1
 
-            @test zcharsvec(data, 2) == _timevars(_zchars)[2:end]
+        @test zcharsvec(data, 2) == _timevars(_zchars)[2:end]
 
-            # iterate through all
-            i,r,l,t = (0,0,0,0)
+        # iterate through all
+        i,r,l,t = (0,0,0,0)
 
-            for unit in data
-                i+= 1
-                @test _i(unit) == i
-                @test isa(unit, DrillUnit)
-                @test DataDrill(unit) === data
-                @test length(unit) == DevelopmentDrilling()
+        for unit in data
+            i+= 1
+            @test _i(unit) == i
+            @test isa(unit, DrillUnit)
+            @test DataDrill(unit) === data
+            @test length(unit) == DevelopmentDrilling()
 
-                for regime in unit
-                    r += 1
-                    @test isa(regime, AbstractDrillRegime)
-                    @test DataDrill(regime) === data
+            for regime in unit
+                r += 1
+                @test isa(regime, AbstractDrillRegime)
+                @test DataDrill(regime) === data
 
-                    for lease in regime
-                        @test isa(lease, DrillLease)
-                        @test DataDrill(lease) === data
-                        l += 1
+                for lease in regime
+                    @test isa(lease, DrillLease)
+                    @test DataDrill(lease) === data
+                    l += 1
 
-                        for obs in lease
-                            t+= 1
-                            @test isa(obs, ObservationDrill)
-                            @test ichars(obs) == (1.0,)
-                            @test _x(obs) == t
-                            @test _y(obs) == t
-                            @test zchars(obs) == _zchars[t]
-                        end
-
+                    for obs in lease
+                        t+= 1
+                        @test isa(obs, ObservationDrill)
+                        @test ichars(obs) == (1.0,)
+                        @test _x(obs) == t
+                        @test _y(obs) == t
+                        @test zchars(obs) == _zchars[t]
                     end
+
                 end
             end
-            @test (i,r,l,t) == (1,2,2,5)
         end
+        @test (i,r,l,t) == (1,2,2,5)
     end
-    @testset "Random Drilling Data" begin
+
+    @testset "Create random DataDrill" begin
 
         num_i = 10
         rho = 0.5
@@ -155,11 +159,7 @@ using ShaleDrillingLikelihood: SimulationDraws, _u, _v, SimulationDrawsMatrix, S
         psi1 = rho .* psi2 .+ (1-rho^2) .* rand(num_i)
 
         leases_per_unit = [sample(minmaxleases) for i in 1:num_i]
-        function randsumtoone(n)
-            x = rand(n)
-            x ./= sum(x)
-            return x
-        end
+
         _jchars = vcat(collect(randsumtoone(lpu) for lpu in leases_per_unit )...)
         num_initial_leases = length(_jchars)
         @test sum(leases_per_unit) == num_initial_leases
