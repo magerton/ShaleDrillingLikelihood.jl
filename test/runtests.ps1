50797696bd1ffcbc9220bcf1532a79e5f37df4f1
Mark <magerton@rice.edu>
2019-11-17 17:10:37 -0800

    fix up runtests.ps1

--- test/runtests.ps1
+++ test/runtests.ps1
@@ -1,4 +1,4 @@
-$Env:JULIA_NUM_THREADS = 4
+echo $Env:JULIA_NUM_THREADS
 $origthreads = $Env:JULIA_NUM_THREADS
 $Env:PATH += ";C:\Users\magerton\AppData\Local\Julia-1.2.0\bin"
 for($i = 1; $i -le $origthreads; $i=$i+1){
