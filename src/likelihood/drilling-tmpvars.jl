01ba49e8c7394f36b8be644050e10b8f7ca70ecf
Mark <magerton@rice.edu>
2019-11-19 07:49:29 -0800

    simplify drilling tmp vars

--- src/likelihood/drilling-tmpvars.jl
+++ src/likelihood/drilling-tmpvars.jl
@@ -9,32 +9,21 @@
 # also
 # https://github.com/nacs-lab/yyc-data/blob/d082032d075070b133fe909c724ecc405e80526a/lib/NaCsCalc/src/utils.jl#L120-L142
 
-function check_lengths(ubv::VV, llj::VV, grad::VV, gradj::VM) where {T,VV<:Vector{Vector{T}}, VM<:Vector{Matrix{T}}}
-    length(theta) == length(ubv) == length(llj) == length(grad)==length(gradj) || throw(DimensionMismatch())
-    all(length.(llj)  .== length(llj[1])  .== size.(gradj, 2)) || throw(DimensionMismatch())
-    all(length.(theta) .== length.(grad) .== length(grad[1]) .== size.(gradj, 1)) || throw(DimensionMismatch())
-end
-
-function check_lengths(ubv::VV, llj::VV, grad::VV, theta::VV, gradj::VM) where {T,VV<:Vector{T}, VM<:Matrix{T}}
-    size(gradj) == (length(grad), length(llj)) || throw(DimensionMismatch())
-    length(grad) == length(theta) || throw(DimensionMismatch())
-end
-
-struct DrillingTmpVars{V<:Vector, VM<:VecOrMat} <: AbstractTmpVars
-    ubv::V
-    llj::V
-    grad::V
-    theta::V
-    gradJ::VM
-    function DrillingTmpVars(ubv::V, llj::V, grad::V, theta::V, gradJ::VM) where {V,VM}
-        # check_lengths(ubv, llj, grad, gradJ)
-        return new{V,VM}(ubv, llj, grad, theta, gradJ)
+struct DrillingTmpVars{T<:AbstractFloat} <: AbstractTmpVars
+    ubv::Vector{T}
+    llj::Vector{T}
+    grad::Vector{T}
+    theta::Vector{T}
+    gradJ::Matrix{T}
+    function DrillingTmpVars(ubv, llj, grad, theta, gradJ)
+        length(theta) == length(grad)==size(gradJ,1) || throw(DimensionMismatch())
+        T = eltype(ubv)
+        return new{T}(ubv, llj, grad, theta, gradJ)
     end
 end
 
-const DrillingTmpVarsAll{T} = DrillingTmpVars{<:Vector{<:Vector{T}}}
-const DrillingTmpVarsThread{T} = DrillingTmpVars{<:Vector{T}}
-
+const DrillingTmpVarsAll{T} = Vector{DrillingTmpVars{T}}
+const DrillingTmpVarsThread = DrillingTmpVars
 
 _ubv(  dtv::DrillingTmpVars) = dtv.ubv
 _llj(  dtv::DrillingTmpVars) = dtv.llj
@@ -42,16 +31,20 @@ _grad( dtv::DrillingTmpVars) = dtv.grad
 _gradJ(dtv::DrillingTmpVars) = dtv.gradJ
 _theta(dtv::DrillingTmpVars) = dtv.theta
 
-_ubv(  dtv::DrillingTmpVarsAll, id) = getindex( _ubv( dtv), id)
-_llj(  dtv::DrillingTmpVarsAll, id) = getindex( _llj( dtv), id)
-_grad( dtv::DrillingTmpVarsAll, id) = getindex( _grad(dtv), id)
-_gradJ(dtv::DrillingTmpVarsAll, id) = getindex(_gradJ(dtv), id)
-_theta(dtv::DrillingTmpVarsAll, id) = getindex(_theta(dtv), id)
+@deprecate _ubv(  dtv::DrillingTmpVarsAll, id) _ubv(  dtv[id])
+@deprecate _llj(  dtv::DrillingTmpVarsAll, id) _llj(  dtv[id])
+@deprecate _grad( dtv::DrillingTmpVarsAll, id) _grad( dtv[id])
+@deprecate _gradJ(dtv::DrillingTmpVarsAll, id) _gradJ(dtv[id])
+@deprecate _theta(dtv::DrillingTmpVarsAll, id) _theta(dtv[id])
 
-length(dtv::DrillingTmpVarsAll) = length(_ubv(dtv))
-
-function getindex(x::DrillingTmpVars{<:Vector}, i)
-    return DrillingTmpVars(_ubv(x,i), _llj(x,i), _grad(x,i), _theta(x,i), _gradJ(x,i))
+function DrillingTmpVars(J::Integer, maxchoices::Integer, k::Integer, T::Type=Float64)
+    tid = T(threadid())
+    ubv = fill(tid, maxchoices)
+    llj = fill(tid, J)
+    grad = fill(tid, k)
+    theta = fill(tid, k)
+    gradJ = fill(tid, k, J)
+    return DrillingTmpVars(ubv, llj, grad, theta, gradJ)
 end
 
 @noinline function DrillingTmpVars(J::Integer, model::AbstractDrillModel, T::Type=Float64)
@@ -59,27 +52,19 @@ end
     maxchoices = num_choices(model)
     k = length(model)
 
-    ubvs  = Vector{Vector{T}}(undef, nth)
-    lljs  = Vector{Vector{T}}(undef, nth)
-    grads = Vector{Vector{T}}(undef, nth)
-    thetas = Vector{Vector{T}}(undef, nth)
-    gradJs = Vector{Matrix{T}}(undef, nth)
+    dtvs = Vector{DrillingTmpVars{T}}(undef, nth)
 
-    let J=J, maxchoices=maxchoices, k=k, nth=nth
+    let J=J, maxchoices=maxchoices, k=k
         @threads for id in OneTo(nth)
-            tid = T(threadid())
-            ubvs[id]  = fill(tid, maxchoices)
-            lljs[id]  = fill(tid, J)
-            grads[id] = fill(tid, k)
-            thetas[id] = fill(tid, k)
-            gradJs[id] = fill(tid, k, J)
+            dtvs[id] = DrillingTmpVars(J, maxchoices, k, T)
         end
     end
 
-    return DrillingTmpVars(ubvs, lljs, grads, thetas, gradJs)
+    return dtvs
 end
 
 update_theta!(dtv::DrillingTmpVarsThread, theta) = (_theta(dtv) .= theta)
+
 function update_theta!(dtv::DrillingTmpVarsAll, theta)
     @threads for i in OneTo(nthreads())
         update_theta!(dtv[i], theta)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,15 @@ using Base.Threads
 
 println("using $(nthreads()) threads")
 
-# include("sum-functions.jl")
-# include("threadutils.jl")
-#
-# include("data/data-structure.jl")
-# include("data/drilling.jl")
-# include("data/overall.jl")
-#
-# include("drilling-model/flow.jl")
-# include("likelihood/royalty.jl")
-# include("likelihood/production.jl")
-# include("likelihood/drilling.jl")
+include("sum-functions.jl")
+include("threadutils.jl")
+
+include("data/data-structure.jl")
+include("data/drilling.jl")
+include("data/overall.jl")
+
+include("drilling-model/flow.jl")
+include("likelihood/royalty.jl")
+include("likelihood/production.jl")
+include("likelihood/drilling.jl")
 include("likelihood/overall.jl")
