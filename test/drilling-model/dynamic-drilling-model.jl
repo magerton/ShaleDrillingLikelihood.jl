6be599ad531dba25a4b1b15d5071829bcebd5f89
Mark <mjagerton@ucdavis.edu>
2019-11-23 12:14:22 -0800

    working on dubv in vfit

--- test/drilling-model/dynamic-drilling-model.jl
+++ test/drilling-model/dynamic-drilling-model.jl
@@ -14,12 +14,16 @@ using ShaleDrillingLikelihood: DynamicDrillingModel,
     DCDPTmpVars,
     ubVfull    ,
     dubVfull   ,
+    dubVfullperm,
     q          ,
     lse        ,
     tmp        ,
     tmp_cart   ,
     Πψtmp      ,
     IminusTEVp,
+    ubV,
+    dubV,
+    dubVperm,
     flow!,
     end_ex0,
     _βΠψ!,
@@ -36,8 +40,18 @@ using ShaleDrillingLikelihood: DynamicDrillingModel,
     _dψ1dρ,
     _dψ1dθρ,
     _ψ1,
-    _ρ
+    _ρ,
+    dp1space,
+    sprimes,
+    _horizon,
+    vfit!,
+    dEV,
+    EV,
+    anticipate_t1ev
 
+const DOBTIME = false
+
+println("print")
 
 @testset "Dynamic Drilling Model" begin
 
@@ -59,6 +73,9 @@ using ShaleDrillingLikelihood: DynamicDrillingModel,
 
     # wp = LeasedProblemContsDrill(dmx,4,5,3,2)
     wp = LeasedProblem(dmx,4,5,3,2)
+
+    # ichars
+    ichar = (2.0, 0.25,)
 
     # ddm object
     ddm = DynamicDrillingModel(f, 0.9, wp, zs, ztrans, ψs, false)
@@ -85,21 +102,21 @@ using ShaleDrillingLikelihood: DynamicDrillingModel,
                 twoh = thetaplus[i] - thetaminus[i]
                 @test maximum(abs.(thetaplus .- thetaminus)) ≈ twoh
 
-                @views tmpvminus = DCDPTmpVars(ubvminus, dubVfull(tmpv), q(tmpv), lse(tmpv), tmp(tmpv), tmp_cart(tmpv), Πψtmp(tmpv), IminusTEVp(tmpv))
-                @views tmpvplus  = DCDPTmpVars(ubvplus,  dubVfull(tmpv), q(tmpv), lse(tmpv), tmp(tmpv), tmp_cart(tmpv), Πψtmp(tmpv), IminusTEVp(tmpv))
+                @views tmpvminus = DCDPTmpVars(ubvminus, dubVfull(tmpv), dubVfullperm(tmpv), q(tmpv), lse(tmpv), tmp(tmpv), tmp_cart(tmpv), Πψtmp(tmpv), IminusTEVp(tmpv))
+                @views tmpvplus  = DCDPTmpVars(ubvplus,  dubVfull(tmpv), dubVfullperm(tmpv), q(tmpv), lse(tmpv), tmp(tmpv), tmp_cart(tmpv), Πψtmp(tmpv), IminusTEVp(tmpv))
 
                 fill!(dubVfull(tmpv), 0)
                 fill!(ubVfull(tmpvminus), 0)
                 fill!(ubVfull(tmpvplus), 0)
 
-                flow!(tmpvminus, ddm, thetaminus, sidx, (2.0, 0.25,), false)
-                flow!(tmpvplus,  ddm, thetaplus, sidx, (2.0, 0.25,), false)
+                flow!(tmpvminus, ddm, thetaminus, sidx, ichar, false)
+                flow!(tmpvplus,  ddm, thetaplus, sidx, ichar, false)
                 @test all(dubVfull(tmpv) .== 0)
 
                 fd[i,:,:,:] .= (ubvplus .- ubvminus)./ twoh
             end
 
-            flow!(tmpv, ddm, theta, sidx, (2.0, 0.25,), true)
+            flow!(tmpv, ddm, theta, sidx, ichar, true)
 
             fdnosig = fd[1:end-1,:,:,:] .- dubVfull(tmpv)[1:end-1,:,:,:]
             fdsig = fd[end,:,:,:] .- dubVfull(tmpv)[end,:,:,:]
@@ -113,7 +130,9 @@ using ShaleDrillingLikelihood: DynamicDrillingModel,
             @test fd ≈ dubVfull(tmpv)
         end
 
-        @btime flow!($tmpv, $ddm, $theta, 2, (2.0, 0.25,), true)
+        if DOBTIME
+            @btime flow!($tmpv, $ddm, $theta, 2, (2.0, 0.25,), true)
+        end
     end
 
     @testset "Learning transition" begin
@@ -153,220 +172,92 @@ using ShaleDrillingLikelihood: DynamicDrillingModel,
             hh = theta_ρ(reward(ddm), theta2) - theta_ρ(reward(ddm), theta1)
             @test hh > 0
 
-            pi1  = similar(Πψtmp(tmpv))
-            pi2  = similar(Πψtmp(tmpv))
-            fdpi = similar(Πψtmp(tmpv))
+            pipsi = Πψtmp(tmpv)
+            fdpi = similar(pipsi)
+            @test fdpi isa Matrix
+            @test !(fdpi === pipsi)
+            pi1 = similar(fdpi)
+            pi2 = similar(fdpi)
 
-            tmpv1 = DCDPTmpVars(ubVfull(tmpv), dubVfull(tmpv), q(tmpv), lse(tmpv), tmp(tmpv), tmp_cart(tmpv), pi1, IminusTEVp(tmpv))
-            tmpv2 = DCDPTmpVars(ubVfull(tmpv), dubVfull(tmpv), q(tmpv), lse(tmpv), tmp(tmpv), tmp_cart(tmpv), pi2, IminusTEVp(tmpv))
+            _βΠψ!(tmpv, ddm, theta1)
+            pi1 .= pipsi
+            _βΠψ!(tmpv, ddm, theta2)
+            pi2 .= pipsi
 
-            _βΠψ!(tmpv1, ddm, theta1)
-            _βΠψ!(tmpv2, ddm, theta2)
+            fdpi .= (pi2 .- pi1) ./ hh
+            @test any(fdpi .!= 0)
             _βΠψdθρ!(tmpv, ddm, theta)
-            fdpi .= -pi1
-            fdpi .+= pi2
-            fdpi ./= hh
 
-            dpi = Πψtmp(tmpv)
-
-            @views maxv, idx = findmax(abs.(dpi .- fdpi))
+            @views maxv, idx = findmax(abs.(pipsi .- fdpi))
             sub = CartesianIndices(fdpi)[idx]
             @show "worst value is $maxv at $sub for β dΠ/dσ"
 
             @test 0.0 < maxv < 1e-5
-            @test fdpi ≈ dpi
+            @test fdpi ≈ pipsi
 
         end
     end
 
+    @testset "VF Iteration" begin
 
+        @testset "Finite horizon gradient" begin
 
+            fdEV = similar(dEV(evs))
+            thetaminus = similar(theta)
+            thetaplus = similar(theta)
 
-    # let EV         = evs.EV,
-    #     dEV        = evs.dEV,
-    #     σ          = σv,
-    #     wp         = prim.wp,
-    #     Πz         = prim.Πz,
-    #     β          = prim.β,
-    #     fdEV       = similar(evs.dEV),
-    #     θ1         = similar(θt),
-    #     θ2         = similar(θt),
-    #     roy        = 0.25,
-    #     geoid      = 4.7,
-    #     itype      = (geoid, roy,),
-    #     nS         = length(prim.wp),
-    #     i          = nS-1,
-    #     idxd       = ShaleDrillingModel.dp1space(wp,i),
-    #     idxs       = collect(ShaleDrillingModel.sprimes(wp,i)),
-    #     horzn      = ShaleDrillingModel._horizon(wp,i)
-    #
-    #     @views ubV =    tmpv.ubVfull[:,:,idxd]
-    #     @views dubV =   tmpv.dubVfull[:,:,:,idxd]
-    #     @views dubV_σ = tmpv.dubV_σ[:,:,idxd]
-    #     @views q      = tmpv.q[:,:,idxd]
-    #
-    #     @views EV0 = EV[:,:,i]
-    #     @views EV1 = EV[:,:,idxs]
-    #     @views dEV0 = dEV[:,:,:,i]
-    #     @views dEV1 = dEV[:,:,:,idxs]
-    #     @views fdEV0 = fdEV[:,:,:,i]
-    #
-    #
-    #     for anticipate_e in (true,false,)
-    #
-    #         tmpv_vw = dcdp_tmpvars(ubV, dubV, dubV_σ, q, tmpv.lse, tmpv.tmp, tmpv.tmp_cart, tmpv.Πψtmp, tmpv.IminusTEVp)
-    #         p = dcdp_primitives(flowfuncname, prim.β, prim.wp, prim.zspace, prim.Πz, prim.ψspace, anticipate_e)
-    #
-    #         @testset "Check finite horizon gradient for anticipate_e = $anticipate_e" begin
-    #             for k = 1:length(θt)
-    #                 h = peturb(θt[k])
-    #                 θ1 .= θt
-    #                 θ2 .= θt
-    #                 θ1[k] -= h
-    #                 θ2[k] += h
-    #                 hh = θ2[k] - θ1[k]
-    #
-    #                 fill!(EV, 0.0)
-    #                 fillflows!(ubV, flow, p, θ1, σ, i, itype...)
-    #                 @test !all(ubV .== 0.0)
-    #                 ubV .+= β .* EV1
-    #                 ShaleDrillingModel.vfit!(EV0, tmpv_vw, p)
-    #                 fdEV0[:,:,k] .= -EV0
-    #
-    #                 fill!(EV, 0.0)
-    #                 fillflows!(ubV, flow, p, θ2, σ, i, itype...)
-    #                 ubV .+= β .* EV1
-    #                 ShaleDrillingModel.vfit!(EV0, tmpv_vw, p)
-    #                 fdEV0[:,:,k] .+= EV0
-    #                 fdEV0[:,:,k] ./= hh
-    #             end
-    #
-    #             fill!(EV, 0.0)
-    #             fill!(dEV, 0.0)
-    #             fillflows!(      ubV, flow,   p, θt, σ, i, itype...)
-    #             fillflows_grad!(dubV, flowdθ, p, θt, σ, i, itype...)
-    #             ubV .+= β .* EV1
-    #             dubV .+= β .* dEV1
-    #             ShaleDrillingModel.vfit!(EV0, dEV0, tmpv_vw, p)
-    #
-    #             println("extrema(dEV0) = $(extrema(dEV0))")
-    #             @views maxv, idx = findmax(abs.(fdEV0 .- dEV0))
-    #             @views sub = CartesianIndices(fdEV0)[idx]
-    #             println("worst value is $maxv at $sub for dθ")
-    #
-    #             @test 0.0 < maxv < 1.0
-    #             @test all(isfinite.(dEV0))
-    #             @test all(isfinite.(fdEV0))
-    #             @test fdEV0 ≈ dEV0
-    #         end
-    #
-    #         @testset "Check vfit/pfit for infinite horizon, anticipate_e = $anticipate_e" begin
-    #             vfEV0 = zeros(size(EV0))
-    #
-    #             fill!(EV, 0.0)
-    #             fillflows!(ubV, flow, p, θt, σ, i, itype...)
-    #             ubV .+= β .* EV1
-    #             converged, iter, bnds = ShaleDrillingModel.solve_inf_vfit!(EV0, tmpv_vw, p; maxit=1000, vftol=1e-10)
-    #             println("vfit done. converged = $converged after $iter iterations. error bounds are $bnds")
-    #             vfEV0 .= EV0
-    #
-    #             fill!(EV, 0.0)
-    #             fillflows!(ubV, flow, p, θt, σ, i, itype...)
-    #             ubV .+= β .* EV1
-    #             converged, iter, bnds = ShaleDrillingModel.solve_inf_vfit_pfit!(EV0, tmpv_vw, p; vftol=1e-10, maxit0=20, maxit1=40)
-    #             println("pfit done. converged = $converged after $iter iterations. error bounds are $bnds")
-    #
-    #             @views maxv, idx = findmax(abs.(vfEV0 .- EV0))
-    #             @views sub = CartesianIndices(EV0)[idx]
-    #             println("worst value is $maxv at $sub for vfit vs pfit")
-    #             @test EV0 ≈ vfEV0
-    #         end
-    #
-    #         @testset "Check gradient for infinite horizon, anticipate_e = $anticipate_e" begin
-    #
-    #             for k = 1:length(θt)
-    #                 h = peturb(θt[k])
-    #                 θ1 .= θt
-    #                 θ2 .= θt
-    #                 θ1[k] -= h
-    #                 θ2[k] += h
-    #                 hh = θ2[k] - θ1[k]
-    #
-    #                 fill!(EV, 0.0)
-    #                 fillflows!(ubV, flow, p, θ1, σ, i, itype...)
-    #                 ubV .+= β .* EV1
-    #                 ShaleDrillingModel.solve_inf_vfit_pfit!(EV0, tmpv_vw, p; vftol=1e-10, maxit0=20, maxit1=40)
-    #                 fdEV0[:,:,k] .= -EV0
-    #
-    #                 fill!(EV, 0.0)
-    #                 fillflows!(ubV, flow, p, θ2, σ, i, itype...)
-    #                 ubV .+= β .* EV1
-    #                 ShaleDrillingModel.solve_inf_vfit_pfit!(EV0, tmpv_vw, p; vftol=1e-10, maxit0=20, maxit1=40)
-    #                 fdEV0[:,:,k] .+= EV0
-    #                 fdEV0[:,:,k] ./= hh
-    #             end
-    #
-    #             fill!(EV, 0.0)
-    #             fill!(dEV, 0.0)
-    #             fillflows!(ubV, flow, p, θt, σ, i, itype...)
-    #             fillflows_grad!(dubV, flowdθ, p, θt, σ, i, itype...)
-    #             ubV .+= β .* EV1
-    #             dubV .+= β .* dEV1
-    #             ShaleDrillingModel.solve_inf_vfit_pfit!(EV0, tmpv_vw, p; vftol=1e-10, maxit0=20, maxit1=40)
-    #             ubV[:,:,1] .= β .* EV0
-    #             ShaleDrillingModel.gradinf!(dEV0, tmpv_vw, p)   # note: destroys ubV & dubV
-    #
-    #             println("extrema(dEV0) = $(extrema(dEV0))")
-    #             @views maxv, idx = findmax(abs.(fdEV0 .- dEV0))
-    #             @views sub = CartesianIndices(fdEV0)[idx]
-    #             println("worst value is $maxv at $sub for dθ")
-    #
-    #             @test 0.0 < maxv < 1.0
-    #             @test all(isfinite.(dEV0))
-    #             @test all(isfinite.(fdEV0))
-    #             @test fdEV0 ≈ dEV0
-    #
-    #         end
-    #     end
-    # end
+            i = 1
 
+            idxd  = dp1space(wp,i)
+            idxs  = collect(sprimes(statespace(ddm),i))
+            horzn = _horizon(wp,i)
 
+            tmp_vw = view(tmpv, idxd)
 
+            EV0   = view(EV(evs) ,    :, :, i)
+            EV1   = view(EV(evs) ,    :, :, idxs)
+            dEV0  = view(dEV(evs), :, :, :, i)
+            dEV1  = view(dEV(evs), :, :, :, idxs)
+            fdEV0 = view(fdEV    , :, :, :, i)
 
+            for k in OneTo(length(theta))
+                h = peturb(theta[k])
+                thetaminus[k] -= h
+                thetaplus[k] += h
+                hh = thetaplus[k] - thetaminus[k]
 
+                fill!(evs, 0)
 
+                flow!(tmp_vw, ddm, thetaminus, i, ichar, false)
+                vfit!(EV0, tmp_vw, ddm)
+                ubV(tmp_vw) .+= discount(ddm) .* EV0
+                fdEV0[:,:,k] .-= EV0
 
+                fill!(evs, 0)
+                flow!(tmp_vw, ddm, thetaplus, i, ichar, false)
+                vfit!(EV0, tmp_vw, ddm)
+                ubV(tmp_vw) .+= discount(ddm) .* EV0
+                fdEV0[:,:,k] .+= EV0
+                fdEV0[:,:,k] ./= hh
+            end
 
+            fill!(evs, 0)
+            flow!(tmp_vw, ddm, thetaplus, i, ichar, true)
+            ubV(tmp_vw)  .+= discount(ddm) .* EV0
+            dubVperm(tmp_vw) .+= discount(ddm) .* dEV1
+            vfit!(EV0, dEV0, tmp_vw, ddm)
 
+            println("extrema(dEV0) = $(extrema(dEV0))")
+            @views maxv, idx = findmax(abs.(fdEV0 .- dEV0))
+            @views sub = CartesianIndices(fdEV0)[idx]
+            println("worst value is $maxv at $sub for dθ")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            @test 0.0 < maxv < 1.0
+            @test all(isfinite.(dEV0))
+            @test all(isfinite.(fdEV0))
+            @test fdEV0 ≈ dEV0
+        end # finite horizon
+    end # vfit
 
 
 
