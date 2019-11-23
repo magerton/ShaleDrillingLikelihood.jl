d200c5fe6f72a83941ced6d8856863ba3c3ec739
Mark <mjagerton@ucdavis.edu>
2019-11-23 12:13:54 -0800

    try permuting dims of dubv

--- src/drilling-model/dynamic-drilling-model.jl
+++ src/drilling-model/dynamic-drilling-model.jl
@@ -61,7 +61,8 @@ struct DCDPEmax{T<:Real}
     EV::Array{T,3}
     dEV::Array{T,4}
     function DCDPEmax(EV, dEV)
-        Base.tail(size(dEV)) == size(EV) || throw(DimensionMismatch())
+         nz, npsi, nk, ns  = size(dEV)
+        (nz, npsi,     ns) == size(EV)  || throw(DimensionMismatch("EV is $(size(EV)) but dEV is $(size(dEV))"))
         new{eltype(EV)}(EV, dEV)
     end
 end
@@ -80,11 +81,12 @@ const dcdp_Emax = DCDPEmax
 
 function DCDPEmax(ddm::DynamicDrillingModel{T}) where {T}
     nz = size(ztransition(ddm), 1)
-    nS = length(statespace(ddm))
     nψ = length(psispace(ddm))
     nK = _nparm(reward(ddm))
-    ev = Array{T,3}(undef, nz, nψ, nS)
-    dev = Array{T,4}(undef, nK, nz, nψ, nS)
+    nS = length(statespace(ddm))
+
+    dev = Array{T,4}(undef, nz, nψ, nK, nS)
+     ev = Array{T,3}(undef, nz, nψ,     nS)
     return DCDPEmax(ev, dev)
 end
 
@@ -116,6 +118,7 @@ end
 struct DCDPTmpVars{T<:Real, SM<:AbstractMatrix{T}, AA3<:AbstractArray3{T}, AA3b<:AbstractArray3{T}, AA4<:AbstractArray4{T}} <: AbstractTmpVars
     ubVfull::AA3
     dubVfull::AA4
+    dubVfullperm::AA4
     q::AA3b
     lse::Matrix{T}
     tmp::Matrix{T}
@@ -123,29 +126,33 @@ struct DCDPTmpVars{T<:Real, SM<:AbstractMatrix{T}, AA3<:AbstractArray3{T}, AA3b<
     Πψtmp::Matrix{T}
     IminusTEVp::SM
 
-    function DCDPTmpVars(ubVfull::AA3, dubVfull::AA4, q::AA3b, lse, tmp, tmp_cart, Πψtmp, IminusTEVp::SM) where {AA3, AA3b, AA4, SM}
+    function DCDPTmpVars(ubVfull::AA3, dubVfull::AA4, dubVfullperm::AA4, q::AA3b, lse, tmp, tmp_cart, Πψtmp, IminusTEVp::SM) where {AA3, AA3b, AA4, SM}
         (nθt, nz, nψ, nd) = size(dubVfull)
+        (     nz, nψ, nθt, nd) == size(dubVfullperm) || throw(DimensionMismatch())
         (nz, nψ, nd) == size(ubVfull) == size(q) || throw(DimensionMismatch())
         (nz,nψ) == size(lse) == size(tmp) == size(tmp_cart) || throw(DimensionMismatch())
         nψ == checksquare(Πψtmp) || throw(DimensionMismatch())
         nz == checksquare(IminusTEVp) || throw(DimensionMismatch())
         T = eltype(ubVfull)
-        return new{T,SM,AA3,AA3b,AA4}(ubVfull, dubVfull, q, lse, tmp, tmp_cart, Πψtmp, IminusTEVp)
+        return new{T,SM,AA3,AA3b,AA4}(ubVfull, dubVfull, dubVfullperm, q, lse, tmp, tmp_cart, Πψtmp, IminusTEVp)
     end
 end
 
 const dcdp_tmpvars = DCDPTmpVars
 
-ubVfull(   x::DCDPTmpVars) = x.ubVfull
-dubVfull(  x::DCDPTmpVars) = x.dubVfull
-q(         x::DCDPTmpVars) = x.q
-lse(       x::DCDPTmpVars) = x.lse
-tmp(       x::DCDPTmpVars) = x.tmp
-tmp_cart(  x::DCDPTmpVars) = x.tmp_cart
-Πψtmp(     x::DCDPTmpVars) = x.Πψtmp
-IminusTEVp(x::DCDPTmpVars) = x.IminusTEVp
-ubV(x::DCDPTmpVars{T,SM,AA3}) where {T,SM,AA3<:SubArray} = ubVfull(x)
-dubV(x::DCDPTmpVars{T,SM,AA3}) where {T,SM,AA3<:SubArray} = dubVfull(x)
+ubVfull(     x::DCDPTmpVars) = x.ubVfull
+dubVfull(    x::DCDPTmpVars) = x.dubVfull
+dubVfullperm(x::DCDPTmpVars) = x.dubVfullperm
+q(           x::DCDPTmpVars) = x.q
+lse(         x::DCDPTmpVars) = x.lse
+tmp(         x::DCDPTmpVars) = x.tmp
+tmp_cart(    x::DCDPTmpVars) = x.tmp_cart
+Πψtmp(       x::DCDPTmpVars) = x.Πψtmp
+IminusTEVp(  x::DCDPTmpVars) = x.IminusTEVp
+
+ubV(     x::DCDPTmpVars{T,SM,AA3}) where {T,SM,AA3<:SubArray} = ubVfull(x)
+dubV(    x::DCDPTmpVars{T,SM,AA3}) where {T,SM,AA3<:SubArray} = dubVfull(x)
+dubVperm(x::DCDPTmpVars{T,SM,AA3}) where {T,SM,AA3<:SubArray} = dubVfullperm(x)
 
 @deprecate _ubVfull(   x::DCDPTmpVars)   ubVfull(   x)
 @deprecate _dubVfull(  x::DCDPTmpVars)   dubVfull(  x)
@@ -161,25 +168,27 @@ dubV(x::DCDPTmpVars{T,SM,AA3}) where {T,SM,AA3<:SubArray} = dubVfull(x)
 size(x::DCDPTmpVars) = size(dubVfull(x))
 
 function fill!(t::DCDPTmpVars, x)
-    fill!(ubVfull(   t), x)
-    fill!(dubVfull(  t), x)
-    fill!(q(         t), x)
-    fill!(lse(       t), x)
-    fill!(tmp(       t), x)
-    fill!(tmp_cart(  t), x)
-    fill!(Πψtmp(     t), x)
+    fill!(ubVfull(     t), x)
+    fill!(dubVfull(    t), x)
+    fill!(dubVfullperm(t), x)
+    fill!(q(           t), x)
+    fill!(lse(         t), x)
+    fill!(tmp(         t), x)
+    fill!(tmp_cart(    t), x)
+    fill!(Πψtmp(       t), x)
 end
 
 function DCDPTmpVars(nθt, nz, nψ, nd, ztransition::AbstractMatrix{T}) where {T<:Real}
-    dubVfull = Array{T,4}(undef, nθt, nz, nψ, nd)
-    ubVfull  = Array{T,3}(undef, nz, nψ, nd)
+    dubVfull = Array{T,4}(undef, nθt, nz, nψ,      nd)
+    dubVperm = Array{T,4}(undef,      nz, nψ, nθt, nd)
+    ubVfull  = Array{T,3}(undef,      nz, nψ,      nd)
     q = similar(ubVfull)
     lse = Matrix{T}(undef, nz, nψ)
     tmp = similar(lse)
     tmp_cart = similar(lse, CartesianIndex{3})
     Πψtmp = Matrix{T}(undef, nψ, nψ)
     IminusTEVp = ensure_diagonal(ztransition)
-    return DCDPTmpVars(ubVfull, dubVfull, q, lse, tmp, tmp_cart, Πψtmp, IminusTEVp)
+    return DCDPTmpVars(ubVfull, dubVfull, dubVperm, q, lse, tmp, tmp_cart, Πψtmp, IminusTEVp)
 end
 
 function DCDPTmpVars(x::DynamicDrillingModel)
@@ -196,8 +205,9 @@ function view(t::DCDPTmpVars, idxd::AbstractVector)
     last(idxd) <= size(ubVfull(t),3) || throw(DomainError())
     @views ubV  = view(ubVfull(t), :,:,  idxd)
     @views dubV = view(dubVfull(t),:,:,:,idxd)
-    @views q    = view(q(t), :,:,idxd)
-    return dcdp_tmpvars(ubV, dubV, q, lse(t), tmp(t), tmp_cart(t), Πψtmp(t), IminusTEVp(t))
+    @views dubvperm = view(dubVfullperm(t),:,:,:,idxd)
+    @views qq   = view(q(t), :,:,idxd)
+    return dcdp_tmpvars(ubV, dubV, dubvperm, qq, lse(t), tmp(t), tmp_cart(t), Πψtmp(t), IminusTEVp(t))
 end
 
 
@@ -208,19 +218,19 @@ end
 function flow!(tmpv::DCDPTmpVars, ddm::DynamicDrillingModel, θ::AbstractVector, sidx::Integer, ichars::Tuple, dograd::Bool)
     zψpdct = product(product(zspace(ddm)...), psispace(ddm))
 
-    k = _nparm(reward(ddm))
+    nk = _nparm(reward(ddm))
     nc = num_choices(statespace(ddm))
 
-    dubv = reshape(dubVfull(tmpv), k, :, nc)
-    ubv  = reshape( ubVfull(tmpv),    :, nc)
+    dubv  = reshape(dubVfull(tmpv),    nk,  :, nc)
+    dubvp = reshape(dubVfullperm(tmpv), :, nk, nc)
+    ubv   = reshape( ubVfull(tmpv),     :, nc)
 
     @threads for dp1 in dp1space(statespace(ddm), sidx)
-
         @inbounds for (i, (z,ψ)) in enumerate(zψpdct)
             obs = ObservationDrill(ddm, ichars, z, dp1-1, sidx)
             grad = view(dubv, :, i, dp1)
             ubv[i,dp1] = flow!(grad, obs, θ, ψ, dograd)
         end
-
     end
+    permutedims!(dubvp, dubv, [2,1,3])
 end
