ec4adb8399521856705c06d452c1b3b4e8732812
Mark <mjagerton@ucdavis.edu>
2019-11-23 19:08:42 -0800

    put tests back

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,8 @@ end
 include("sum-functions.jl")
 include("threadutils.jl")
 
-include("data/data-structure.jl") # include("data/drilling.jl")
+include("data/data-structure.jl")
+include("data/drilling.jl")
 include("data/overall.jl")
 
 include("drilling-model/state-space.jl")
@@ -20,5 +21,6 @@ include("drilling-model/drilling-flow.jl")
 
 include("drilling-model/dynamic-drilling-model.jl")
 
+include("drilling-model/dynamic-drilling-learning-versions.jl")
 include("likelihood/drilling.jl")
 include("likelihood/overall.jl")
