ba6b5b503c8b2cf68d9b3d71c55243e685b9f96a
Mark <magerton@rice.edu>
2019-12-06 08:22:10 -0800

    tests check out!

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,21 +7,21 @@ module CompileShaleDrillingLikelihood
     using ShaleDrillingLikelihood
 end
 
-# include("sum-functions.jl")
-# include("threadutils.jl")
-# include("inplace-interpolation.jl")
-#
-# include("data/data-structure.jl")
-# include("data/drilling.jl")
-# include("data/overall.jl")
-#
-# include("drilling-model/test-flow.jl")
-#
-# include("drilling-model/state-space.jl")
-# include("drilling-model/drilling-flow.jl")
-# include("drilling-model/dynamic-drilling-model.jl")
+include("sum-functions.jl")
+include("threadutils.jl")
+include("inplace-interpolation.jl")
+
+include("data/data-structure.jl")
+include("data/drilling.jl")
+include("data/overall.jl")
+
+include("drilling-model/test-flow.jl")
+
+include("drilling-model/state-space.jl")
+include("drilling-model/drilling-flow.jl")
+include("drilling-model/dynamic-drilling-model.jl")
 include("drilling-model/test-full-payoff.jl")
-# include("drilling-model/dynamic-drilling-learning-versions.jl")
-#
-# include("likelihood/drilling.jl")
-# include("likelihood/overall.jl")
+include("drilling-model/dynamic-drilling-learning-versions.jl")
+
+include("likelihood/drilling.jl")
+include("likelihood/overall.jl")
