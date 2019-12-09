6dbccd1b6425b8ebc2d9e192d65a0fca357c3176
Mark <magerton@rice.edu>
2019-12-08 22:05:04 -0800

    buried in housekeeping of generating data

--- test/drilling-model/data-simulation.jl
+++ test/drilling-model/data-simulation.jl
@@ -84,6 +84,7 @@ println("print to keep from blowing up")
     # for balanced panel of states + decisions
     lease_states    = zeros(Int, num_zt, total_leases(data))
     lease_decisions = zeros(Int, num_zt, total_leases(data))
+    lease_outcome   = Vector{Symbol}(undef, total_leases(data))
 
     # for start/end times of each lease
     tdrill          = zeros(Int,      total_leases(data))
@@ -150,6 +151,7 @@ println("print to keep from blowing up")
             # update lease_j
             else
                 lease_decisions[:,j] .= y_jselect   # update other decisions
+                tdrill[j] = tdrill_jselect
                 for t in jtstart(data,j):size(lease_states,1)-1
                     st = lease_states[t,j]
                     d = lease_decisions[t,j]
@@ -176,6 +178,139 @@ println("print to keep from blowing up")
 # 3. push lease_states[jtstart(data,j):tdrill] to tchars
 #      also update tptr, j1ptr
 # 4. for selected lease, push to tptr, tchars
+
+@test vec(mapslices( x -> all(x.==-1), lease_decisions; dims=1)) == vec(mapslices( x -> any(x.<0), lease_decisions; dims=1))
+@test vec(mapslices( x -> all(x.==-1), lease_states; dims=1)) == vec(mapslices( x -> any(x.<0), lease_states; dims=1))
+@test vec(mapslices( x -> all(x.==-1), lease_states; dims=1)) == vec(mapslices( x -> any(x.<0), lease_decisions; dims=1))
+
+
+keep_lease = vec(mapslices( x -> all(x.>=0), lease_decisions; dims=1))
+num_wells  = vec(mapslices(sum, lease_decisions; dims=1))
+tstop1 = zeros(Int, total_leases(data))
+tstop2 = zeros(Int, total_leases(data))
+all(keep_lease[selected_initial_leases]) || throw(error("not keeping selected leases. boo!"))
+
+
+tdrill_first    = fill(num_zt+1, total_leases(data))
+tdrill_complete = fill(num_zt+1, total_leases(data))
+texpire         = zeros(Int, total_leases(data))
+
+# create balance "panel"
+last_state = length(wp)
+expired_state = exploratory_terminal(wp)
+
+for unit in data
+    for lease in InitialDrilling(unit)
+        j = _i(lease)
+        decisions = view(lease_decisions, :, j)
+        states = view(lease_states, :, j)
+
+        # if ever drilled
+        if sum(decisions) > 0
+            tstop1[j] = findfirst(d -> d > 0, decisions)
+
+            # totally drilled
+            if last_state in states
+                tstop2[j] = findfirst(s -> s == last_state, states)-1
+
+            # not totally drilled
+            else
+                tstop2[j] = length(states)
+            end
+
+        # else never drilled
+        else
+            # lease expires
+            if expired_state in states
+                tstop1[j] = findfirst(s -> s == expired_state, states)
+            # or not
+            else
+                tstop1[j] = length(states)
+            end
+            tstop2[j] = tstop1[j]
+        end
+    end
+end
+
+ttimes1 = [a:b for (a,b) in zip(jtstart(data)[1:total_leases(data)], tstop1)]
+ttimes2 = [a+1:b for (a,b) in zip(tstop1, tstop2)]
+
+all_same_value(x) = all(x .== x[1])
+
+for unit in data
+    i = _i(unit) # unit number
+    jrng = j1_range(data, i)
+
+    tstop1s = tstop1[jrng]
+    keeps = keep_lease[jrng]
+    drilled = num_wells[jrng] .> 0
+    if any(drilled[keeps])
+        @test all_same_value(tstop1s[keeps])
+    else
+        @test all(length.( ttimes2[jrng][keeps] ) .== 0)
+    end
+end
+
+
+num_i = length(data)
+_tptr  = 1 .+ cumsum(vcat(0, length.(ttimes1[keep_lease]), length.(ttimes2[selected_initial_leases])))
+_j1ptr = 1 .+ cumsum(vcat(0, [sum(keep_lease[j1_range(data,i)]) for i in 1:num_i]))
+_j2ptr = (last(_j1ptr)-1) .+ (1:num_i)
+nobs = last(_tptr)-1
+_jtstart = vcat(first.(ttimes1[keep_lease]), first.(ttimes2[selected_initial_leases]))
+
+xnew = zeros(Int, nobs)
+ynew = zeros(Int, nobs)
+
+for i = 1:num_i
+    jnewrng = _j1ptr[i]:_j1ptr[i+1]-1
+
+    keeps = keep_lease[j1_range(data,i)]
+    joldrng = collect(j1_range(data,i))[keeps]
+    tstop1s = tstop1[joldrng]
+
+    for (jidx, jnew) in enumerate(jnewrng)
+        tnewrng = _tptr[jnew]:_tptr[jnew+1]-1
+        toldrng = tstop1s[jidx]
+        jolddidx = joldrng[jidx]
+        xnew[tnewrng] .= lease_states[toldrng, jolddidx]
+        ynew[tnewrng] .= lease_decisions[toldrng, jolddidx]
+    end
+end
+
+ynew
+
+
+unitids = zeros(Int, total_leases(data))
+for i in 1:length(data)
+    unitids[j1_range(data,i)] .= i
+end
+
+(unitids[keep_lease], ttimes1[keep_lease], ttimes2[keep_lease])
+(unitids, vec(mapslices(sum, lease_decisions; dims=1)), ttimes1, ttimes2)
+
+
+
+    jselect = selected_initial_leases[i] # pick this lease
+
+
+
+    y_jselect      = lease_decisions[:,jselect]  # pick these decisions
+    x_jselect      = lease_states[:,jselect]     # pick this set of states
+    tend_jselect   = tend[jselect]               # ending obs
+    tdrill_jselect = tdrill[jselect]
+
+
+
+end
+
+
+
+for j in 1:total_leases(data)
+    if any(lease_decisions[:,j] .< 0 )
+        all(lease_decisions[:,j] .== -1) || throw(error())
+    end
+end
 
 lease_states[:,selected_initial_leases[2]]
 
