226958ec874d5bca025af7e30d95bd79d59e1e32
Mark <magerton@rice.edu>
2019-12-15 23:41:00 -0800

    Huge speedup by dropping threading
    ----------------------------------------------
    Using 1 threads and adding 8 workers
    ----------------------------------------------
      2.615 s (14826 allocations: 472.69 KiB)
      3.778 s (14858 allocations: 473.81 KiB)
    Julia 1.2.0
    Simulating 150 units.
    Progress: 100%|███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| Time: 0:00:11
    ----------------------------------------------
    Using 1 threads and adding 8 workers
    ----------------------------------------------
      2.615 s (16339 allocations: 487.89 KiB)
      4.283 s (16404 allocations: 489.98 KiB)
    Julia 1.3.0
    Simulating 150 units.
    Progress: 100%|███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| Time: 0:00:12
    ----------------------------------------------
    Using 1 threads and adding 8 workers
    ----------------------------------------------
      2.511 s (15852 allocations: 477.14 KiB)
      3.725 s (15815 allocations: 474.48 KiB)

--- test/speedtests.ps1
+++ test/speedtests.ps1
@@ -1,9 +1,10 @@
 echo $Env:JULIA_NUM_THREADS
 $origthreads = $Env:JULIA_NUM_THREADS
 # $Env:PATH += ";C:\Users\magerton\AppData\Local\Julia-1.3.0\bin"
-foreach ($i in 1) {
+foreach ($i in 1,2) {
     $env:JULIA_NUM_THREADS = $i
-    
+    echo "Using $env:i threads"
+        
     echo "Julia 1.1.1"
     C:\Users\magerton\AppData\Local\Julia-1.1.1\bin\julia.exe ".\full-model\optimize-dynamic-speedtest.jl"
     
