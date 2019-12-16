b14fdf6f55e7ab7f235fd5fe26846bded2bcdd8d
Mark <magerton@rice.edu>
2019-12-15 21:49:19 -0800

    bash script for speedtests?

---
+++ test/speedtests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo $JULIA_NUM_THREADS
+ORIGTHREADS=$JULIA_NUM_THREADS
+for i in 1 2 4 8 16 32 64
+do
+    export JULIA_NUM_THREADS=$i
+    julia "./full-model/optimize-dynamic-speedtest.jl"
+end
+export JULIA_NUM_THREADS=$ORIGTHREADS
