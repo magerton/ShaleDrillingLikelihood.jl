37c5f2122a28245e59c8edbe52ba6f7b65e0b4ee
Mark <magerton@rice.edu>
2019-12-15 22:06:22 -0800

    Julia 1.1.1 on speedtests

--- test/speedtests.ps1
+++ test/speedtests.ps1
@@ -1,7 +1,8 @@
 echo $Env:JULIA_NUM_THREADS
 $origthreads = $Env:JULIA_NUM_THREADS
-$Env:PATH += ";C:\Users\magerton\AppData\Local\Julia-1.2.0\bin"
+$Env:PATH += ";C:\Users\magerton\AppData\Local\Julia-1.1.1\bin"
 foreach ($i in 1,2,4,8) {
     $env:JULIA_NUM_THREADS = $i
     julia.exe ".\full-model\optimize-dynamic-speedtest.jl"
 }
+$env:JULIA_NUM_THREADS = $origthreads
