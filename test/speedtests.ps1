338a4ebb84e062699c0e376095d503ecf65061e6
Mark <magerton@rice.edu>
2019-12-15 22:37:53 -0800

    speedtest over julia versions

--- test/speedtests.ps1
+++ test/speedtests.ps1
@@ -1,8 +1,16 @@
 echo $Env:JULIA_NUM_THREADS
 $origthreads = $Env:JULIA_NUM_THREADS
-$Env:PATH += ";C:\Users\magerton\AppData\Local\Julia-1.1.1\bin"
-foreach ($i in 1,2,4,8) {
+# $Env:PATH += ";C:\Users\magerton\AppData\Local\Julia-1.3.0\bin"
+foreach ($i in 1 2) {
     $env:JULIA_NUM_THREADS = $i
-    julia.exe ".\full-model\optimize-dynamic-speedtest.jl"
+    
+    echo "Julia 1.1.1"
+    C:\Users\magerton\AppData\Local\Julia-1.1.1\bin\julia.exe ".\full-model\optimize-dynamic-speedtest.jl"
+    
+    echo "Julia 1.2.0"
+    C:\Users\magerton\AppData\Local\Julia-1.2.0\bin\julia.exe ".\full-model\optimize-dynamic-speedtest.jl"
+    
+    echo "Julia 1.3.0"
+    C:\Users\magerton\AppData\Local\Julia-1.3.0\bin\julia.exe ".\full-model\optimize-dynamic-speedtest.jl"
 }
 $env:JULIA_NUM_THREADS = $origthreads
