64fbf0f1997c8d2408f3ba368cfd9d2fd7b5bb5c
Mark <magerton@rice.edu>
2019-12-17 10:37:45 -0800

    ready to test on FARM?

--- example/run_estimator.jl
+++ example/run_estimator.jl
@@ -6,29 +6,19 @@ using ShaleDrillingLikelihood.SDLParameters
 if "SLURM_JOBID" in keys(ENV)
     using ClusterManagers
 end
-using CountPlus, Optim, Test, Distributed
-
-using DataFrames, CategoricalArrays, Dates, StatsModels, Query
-using FileIO: load
-
-using Optim: minimizer
+using CountPlus, Distributed, JLD2
+using Optim: minimizer, Options
 
 # ------------------- number of simulations ----------------------
 
-M_dcdp_dfree       = 175
-M_dcdp_bfgs        = 250
-M_full_dfree       = 175
-M_full_bfgs        = 500
+M_cnstr = 250
+M_full  = 500
 
-do_dcdp_dfree      = false
-do_dcdp_bfgs       = true
-do_full_dfree      = false
-do_full_bfgs       = true
+do_cnstr = true
+do_full  = true
 
-maxtime_dcdp_dfree = Int(   0.5 * 60^2)
-maxtime_dcdp_bfgs  = Float64( 1 * 60^2)
-maxtime_full_dfree = Int(     2 * 60^2)
-maxtime_full_bfgs  = Float64(48 * 60^2)
+maxtime_cnstr = 1 * 60^2
+maxtime_full  = 1 * 60^2
 
 REWARD = DrillReward(
     DrillingRevenue(Unconstrained(), NoTrend(), GathProcess() ),
@@ -36,44 +26,50 @@ REWARD = DrillReward(
     ExtensionCost_Constant()
 )
 
-# # --------------- function -------------------
-#
-# function testsort(d::Dict, tbls::NTuple{N,String}, cols::NTuple{N,Symbol}) where {N}
-#     for i = 1:N
-#         tbl = tbls[i]
-#         col = cols[i]
-#         issorted(d[tbl][!,col]) || throw(error("table $tbl not sorted on $col!"))
-#     end
-#     println("tables are sorted")
-# end
-#
-# --------------- load in data ---------------
+ANTICIPATE = true
+PSI = PsiSpace(21)
+NUM_P = 21
+NUM_C = 13
+EXTEND_GRID = log(3)
+MINP = minp_default()
+DISCOUNT = RealDiscountRate()
 
-datadir = "E:/projects/haynesville/intermediate_data"
-datapath = "data_all_leases.RData"
-rdatapath = joinpath(datadir, datapath)
-data_rdata = load(rdatapath)
+DATADIR = "~/haynesville/intermediate_data"
+DATAPATH = "data_all_leases.RData"
 
+# --------------- create data ---------------
 
-rwrd = REWARD
-DataRoyalty(rwrd, rdatapath)
-DataProduce(rwrd, rdatapath)
-DataDrillPrimitive(rwrd, rdatapath)
+# constrained version of reward function
 
-# ---------------------------------------
-#
-# REWARD =  = DrillReward(
-#     DrillingRevenue(Unconstrained(), TimeTrend(), GathProcess() ),
-#     DrillingCost_TimeFE(2008,2012), # DrillingCost_constant(),
-#     ExtensionCost_Constant()
-# )
-# ANTICIPATE_E = true
-# CONSTR_COEFS = (log_ogip=0x1.47479e0efc894p-1, α_ψ=0x1.4388dea893293p-2, α_t=0x1.cf5fe057db0cfp-6, )
-#
-# Z_INFO = ShaleDrillingEstimation.PriceProcess.ProblemParms(;nlogp = 15, nlogc = 15, ngeology = 15, nψ = 15)
-#
-# println_time_flush("Data set")
+# load in data from disk
+rdatapath = joinpath(DATADIR, DATAPATH)
+data_royalty    = DataRoyalty(REWARD, rdatapath)
+data_produce    = DataProduce(REWARD, rdatapath)
+data_drill_prim = DataDrillPrimitive(REWARD, rdatapath)
 
+thetarho0 = 1.178623255316409
+theta0_royalty = Theta(data_royalty; θρ = thetarho0)
+theta0_produce = Theta(data_produce)
+theta0_drill   = Theta(REWARD; θρ=thetarho0)
+theta0s = (theta0_drill, theta0_royalty, theta0_produce)
+
+# transition matrices
+zrng, ztrans = GridTransition(data_drill_prim, EXTEND_GRID, NUM_P; minp=MINP)
+
+# create drilling dataset
+wp = statespace(data_drill_prim)
+ddm = DynamicDrillModel(REWARD, DISCOUNT, wp, zrng, ztrans, PSI, ANTICIPATE)
+data_drill = DataDrill(ddm, data_drill_prim)
+
+# full dataset
+dataset_full = DataSetofSets(data_drill, data_royalty, data_produce, CoefLinks(data_drill))
+theta0_full = merge_thetas(theta0s, dataset_full)
+
+# constrained model
+rwrd_cnstr = ConstrainedProblem(REWARD, theta0_drill)
+theta0_cnstr = ThetaConstrained(REWARD, theta0_drill)
+data_cnstr = DataDrill(DynamicDrillModel(rwrd_cnstr, ddm), data_drill)
+dataset_cnstr = DataSetofSets(data_cnstr, EmptyDataSet(), EmptyDataSet())
 
 # ------------------- set up workers -----------------------
 
@@ -81,20 +77,21 @@ rmprocs(getworkers())
 pids = addprocs()
 @everywhere using ShaleDrillingLikelihood
 
-let theta=theta0, M=M, maxtime
+let d=dataset_cnstr, theta=theta0_cnstr, M=M_cnstr, maxtime=maxtime_cnstr
     # estimation objects
     leo = LocalEstObj(d, theta)
     reo = RemoteEstObj(leo, M)
     ew = EstimationWrapper(leo, reo)
     leograd = ShaleDrillingLikelihood.grad(leo)
+    @eval @everywhere set_g_RemoteEstObj($reo)
 
     resetcount!()
-    startcount!([50, 500, 100000,], [1, 5, 100,])
+    startcount!([100, 500, 100000,], [1, 5, 100,])
     opts = Optim.Options(show_trace=true, time_limit=maxtime, allow_f_increases=true)
 
     res = solve_model(ew, theta; OptimOpts=opts)
 
-    println(res)
+    # println(res)
     println("Recomputing final gradient / hessian")
     let dograd=true, theta=minimizer(res)
         parallel_simloglik!(ew, theta, dograd)
