932d2e0c3dc421f0b16c46b73648735f0962a795
Mark Agerton <mjagerton@ucdavis.edu>
2019-12-17 23:34:16 -0800

    prep for SLURM

--- example/run_estimator.jl
+++ example/run_estimator.jl
@@ -11,19 +11,19 @@ using Optim: minimizer, Options
 
 # ------------------- number of simulations ----------------------
 
-M_cnstr = 50
-M_full  = 50
+M_cnstr = 500
+M_full  = 500
 
 do_cnstr = true
 do_full  = true
 
-maxtime_cnstr = 1 # 2 * 60^2
-maxtime_full  = 1 # 3 * 60^2
+maxtime_cnstr = 2 * 60^2
+maxtime_full  = 3 * 60^2
 
 REWARD = DrillReward(#
     DrillingRevenue(Unconstrained(), NoTrend(), GathProcess() ),
-    # DrillingCost_TimeFE(2008,2012),
-    DrillingCost_constant(),
+    DrillingCost_TimeFE(2008,2012),
+    # DrillingCost_constant(),
     ExtensionCost_Constant()
 )
 
@@ -35,8 +35,8 @@ EXTEND_GRID = log(3)
 MINP = minp_default()
 DISCOUNT = RealDiscountRate()
 
-DATADIR = "E:/projects/haynesville/intermediate_data"
-# DATADIR = "/home/magerton/haynesville/intermediate_data"
+# DATADIR = "E:/projects/haynesville/intermediate_data"
+DATADIR = "/home/magerton/haynesville/intermediate_data"
 DATAPATH = "data_all_leases.RData"
 
 # --------------- create data ---------------
