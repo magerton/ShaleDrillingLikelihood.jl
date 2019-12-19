757091cf70760df7c95744f607bc59d634ea6c45
Mark Agerton <mjagerton@ucdavis.edu>
2019-12-19 14:40:03 -0600

    do riunestimotor for rigrate

--- example/run-estimator-rigs.jl
+++ example/run-estimator-rigs.jl
@@ -8,41 +8,52 @@ if "SLURM_JOBID" in keys(ENV)
 end
 using CountPlus, Distributed
 using Optim: minimizer, Options, BFGS, NelderMead
-using ShaleDrillingLikelihood: value_function, EVobj
+using ShaleDrillingLikelihood: value_function, EVobj, cost
+using SparseArrays: nonzeros
+using Formatting: generate_formatter
+
+num_nonzeros(x) = length(nonzeros(x))
+intstr = generate_formatter("%'d")
 
 # ------------------- number of simulations ----------------------
 
 M_cnstr = 500
 M_full  = 1000
 
-DO_CNSTR = true # true
+DO_CNSTR = false
 DO_FULL  = true
 
-COMPUTE_INITIAL_VALUES = true
+COMPUTE_INITIAL_VALUES = false
 
-maxtime_cnstr = 3 * 60^2
-maxtime_nelder = 1 * 60^2
-maxtime_full  = 8 * 60^2
+MAXTIME_CNSTR = 3 * 60^2
+MAXTIME_FULL  = 48 * 60^2
 
-REWARD = DrillReward(
-    DrillingRevenue(Unconstrained(), TimeTrend(), GathProcess() ),
-    # DrillingCost_TimeFE(2008,2012),
-    DrillingCost_TimeFE_rigrate(2008,2012),
-    # DrillingCost_dgt1(),
-    ExtensionCost_Constant()
-)
-println("Model is\n")
-println(REWARD)
-println("")
+THETA0_FULL_OVERRIDE = [-0x1.5744a54d60f62p+3, -0x1.d117aa07eef64p+2, -0x1.79f927f9bb9ffp+2, -0x1.441a10e192652p+2, -0x1.2acdb6e2bada1p+2, 0x1.9354425393b3cp+0, -0x1.782a92f1ee656p+0, -0x1.a3bab435dc7d7p+0, -0x1.4e03895da1d3ep+1, 0x1.316a8fef388e9p-1, 0x1.65c962aad6d6bp-2, 0x1.bcc7b91882082p-7, 0x1.527e753d41bep-1, 0x1.0c1cb074832d1p-3, 0x1.3137f3e64d182p-1, 0x1.2ffe4305080fap+0, -0x1.b6c5a5ac5414p+0, 0x1.1e815f092437cp-3, 0x1.e87168cddfdffp+1, 0x1.09aca495e0e38p+2, 0x1.405a56f44902cp+2, 0x1.7a0727a91f6d9p+2, 0x1.9eef181e16c0ap+2, -0x1.d8872e15169d5p+3, 0x1.8c727c253f856p-4, 0x1.481ac7a4734e9p-2, ]
+
+COST = DrillingCost_TimeFE_rigrate(2008,2012)
+EXT = ExtensionCost_Constant()
+REV = DrillingRevenue(Unconstrained(), TimeTrend(), GathProcess() )
+REWARD = DrillReward(REV, COST, EXT)
+println("Model is\n\t$REWARD\n")
+
+
+if cost(REWARD) isa DrillingCost_TimeFE
+    PSI = PsiSpace(51)
+    NUM_P = 51
+    EXTEND_GRID = log(3)
+    MINP = minp_default()
+elseif cost(REWARD) isa DrillingCost_TimeFE_rigrate
+    PSI = PsiSpace(13)
+    NUM_P = 13
+    EXTEND_GRID = log(2.5)
+    MINP = 5e-5
+end
 
 ANTICIPATE = false
-PSI = PsiSpace(13)
-NUM_P = 13
-NUM_C = 13
-EXTEND_GRID = log(2.5)
-MINP = minp_default()
+println("Firms anticipate T1EV shocks? $ANTICIPATE")
+
 DISCOUNT = RealDiscountRate()
-println("Psi-space is\n\t$PSI")
+println("Psi-space is\n\t$PSI with length $(length(PSI))")
 
 if "SLURM_JOBID" in keys(ENV)
     DATADIR = "/home/magerton/haynesville/intermediate_data"
@@ -52,8 +63,6 @@ end
 DATAPATH = "data_all_leases.RData"
 
 # --------------- create data ---------------
-
-# constrained version of reward function
 
 # load in data from disk
 rdatapath = joinpath(DATADIR, DATAPATH)
@@ -69,7 +78,8 @@ theta0_drill   = Theta(REWARD; θρ=thetarho0)
 
 # transition matrices
 zrng, ztrans = GridTransition(data_drill_prim, EXTEND_GRID, NUM_P; minp=MINP)
-println_time_flush("Z range is\n\t$zrng")
+println_time_flush("Z range is\n\t$zrng with lengths $(length.(zrng))")
+println_time_flush("Transition matrix drops probs < $MINP and has $(intstr(num_nonzeros(ztrans))) elements")
 
 # create drilling dataset
 wp = statespace(data_drill_prim)
@@ -77,7 +87,7 @@ ddm = DynamicDrillModel(REWARD, DISCOUNT, wp, zrng, ztrans, PSI, ANTICIPATE)
 data_drill = DataDrill(ddm, data_drill_prim)
 
 evdims = size(EVobj(value_function(ddm)))
-println_time_flush("EV dimension is $evdims, implying $(prod(evdims)) states")
+println_time_flush("EV dimension is $evdims, implying $(intstr(prod(evdims))) states")
 
 # full dataset
 dataset_full = DataSetofSets(data_drill, data_royalty, data_produce, CoefLinks(data_drill))
@@ -108,7 +118,7 @@ println_time_flush("Library loaded on workers")
 
 # Solve constrained simpler model
 if DO_CNSTR
-    res_c, ew_c = solve_model(dataset_cnstr, theta0_cnstr, M_cnstr, maxtime_cnstr)
+    res_c, ew_c = solve_model(dataset_cnstr, theta0_cnstr, M_cnstr, MAXTIME_CNSTR)
     theta1_cnstr = minimizer(res_c)
     updateThetaUnconstrained!(REWARD, theta0_drill, theta1_cnstr)
 end
@@ -117,7 +127,11 @@ end
 theta0s = (theta0_drill, theta0_royalty, theta0_produce)
 theta0_full = merge_thetas(theta0s, dataset_full)
 
+if length(THETA0_FULL_OVERRIDE) == length(theta0_full)
+    theta0_full .= THETA0_FULL_OVERRIDE
+end
+
 if DO_FULL
     println("Starting full model solution")
-    res_u, ew_u = solve_model(dataset_full, theta0_full, M_full, maxtime_full)
+    res_u, ew_u = solve_model(dataset_full, theta0_full, M_full, MAXTIME_FULL)
 end
