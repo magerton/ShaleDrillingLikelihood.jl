4dc28007c81a04af5f5eb2c65be9f4a99e8bd650
Mark Agerton <mjagerton@ucdavis.edu>
2019-12-24 12:47:05 -0600

    Revert "add scratch cmdline args, runextimator-norigs"
    This reverts commit 83034d0e361a838125997a661832cde3769e580d.

--- example/cmdline-args.jl
+++
@@ -1,8 +0,0 @@
-using Revise
-using ShaleDrillingLikelihood
-using ShaleDrillingLikelihood.SDLParameters
-
-parsed_args = parse_commandline()
-
-println(keys(parsed_args))
-print_parsed_args(parsed_args)
