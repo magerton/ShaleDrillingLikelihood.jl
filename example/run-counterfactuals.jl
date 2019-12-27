33f571dbdd6bb9d269e13afe39e3d12eb522ea95
Mark Agerton <mjagerton@ucdavis.edu>
2019-12-27 07:49:30 -0600

    run ONLY the baseline simulation

--- example/run-counterfactuals.jl
+++ example/run-counterfactuals.jl
@@ -85,10 +85,6 @@ end
 
 # ------------------- simulations -----------------------
 
-R = ShaleDrillingLikelihood.revenue(REWARD)
-C = ShaleDrillingLikelihood.cost(REWARD)
-E = ShaleDrillingLikelihood.extend(REWARD)
-
 wp = statespace(ddm)
 PP = PerpetualProblem(wp)
 
@@ -97,6 +93,9 @@ PP = PerpetualProblem(wp)
 cost_df = average_cost_df(dataset_full, theta)
 
 # ------------------- simulation info -----------------------
+R = ShaleDrillingLikelihood.revenue(REWARD)
+C = ShaleDrillingLikelihood.cost(REWARD)
+E = ShaleDrillingLikelihood.extend(REWARD)
 
 DrillRev(lrn, roy) = DrillingRevenue(constr(R), tech(R), tax(R), lrn, roy)
 DrillRwrd(lrn, roy) = DrillReward(DrillRev(lrn,roy), C, E)
@@ -105,19 +104,20 @@ theta_notech = Theta_NoTech(dataset_full, theta, TECH_YEAR_ZERO)
 TSTOP = time_idx(zchars(drill(dataset_full)), DateQuarter(DATE_STOP))
 
 simlist = [
-    (DrillRwrd( learn(R),      royalty(R) ),   wp, theta, ),           # "Baseline"
-    (DrillRwrd( learn(R),      royalty(R) ),   wp, theta_notech, ),    # "Where to drill"
-    (DrillRwrd( NoLearn(),     royalty(R) ),   wp, theta, ),           # "No update ($\\psi_{it} = \\psi_i^0$)"
-    (DrillRwrd( PerfectInfo(), royalty(R) ),   wp, theta, ),           # "Perfect information ($\\psi_{it} = \\psi_i^1$)"
-    (DrillRwrd( MaxLearning(), royalty(R) ),   wp, theta, ),           # "Uninformative signals ($\\rho = 0$)"
-    (DrillRwrd( learn(R),      NoRoyalty()),   wp, theta, ),           # "No royalty"
-    (DrillRwrd( learn(R),      royalty(R) ),   PP, theta, ),           # "No expiration"
-    (DrillRwrd( learn(R),      NoRoyalty()),   PP, theta, ),           # "Ownership"
-    (DrillRwrd(NoLearn(),      NoRoyalty()  ), PP, theta_notech, ),    # "Price only"
-    (DrillRwrd(NoLearn(),      NoRoyalty()  ), PP, theta, ),           # "Price + tech (no distortions)"
-    (DrillRwrd(NoLearn(),      WithRoyalty()), wp, theta_notech, ),    # "Price + leases"
-    (DrillRwrd(Learn(),        NoRoyalty()  ), PP, theta_notech, ),    # "Price + learning"
-    (DrillRwrd(NoLearn(),      WithRoyalty()), PP, theta, ),           # "How to drill"
+    # (DrillRwrd( learn(R),      royalty(R) ),   wp, theta, ),           # "Baseline"
+    (reward(ddm), wp, theta),
+    # (DrillRwrd( learn(R),      royalty(R) ),   wp, theta_notech, ),    # "Where to drill"
+    # (DrillRwrd( NoLearn(),     royalty(R) ),   wp, theta, ),           # "No update ($\\psi_{it} = \\psi_i^0$)"
+    # (DrillRwrd( PerfectInfo(), royalty(R) ),   wp, theta, ),           # "Perfect information ($\\psi_{it} = \\psi_i^1$)"
+    # (DrillRwrd( MaxLearning(), royalty(R) ),   wp, theta, ),           # "Uninformative signals ($\\rho = 0$)"
+    # (DrillRwrd( learn(R),      NoRoyalty()),   wp, theta, ),           # "No royalty"
+    # (DrillRwrd( learn(R),      royalty(R) ),   PP, theta, ),           # "No expiration"
+    # (DrillRwrd( learn(R),      NoRoyalty()),   PP, theta, ),           # "Ownership"
+    # (DrillRwrd(NoLearn(),      NoRoyalty()  ), PP, theta_notech, ),    # "Price only"
+    # (DrillRwrd(NoLearn(),      NoRoyalty()  ), PP, theta, ),           # "Price + tech (no distortions)"
+    # (DrillRwrd(NoLearn(),      WithRoyalty()), wp, theta_notech, ),    # "Price + leases"
+    # (DrillRwrd(Learn(),        NoRoyalty()  ), PP, theta_notech, ),    # "Price + learning"
+    # (DrillRwrd(NoLearn(),      WithRoyalty()), PP, theta, ),           # "How to drill"
 ]
 
 simlist_meta = [(a,b,copy(theta_revenue(dataset_full, c))) for (a,b,c) in simlist]
