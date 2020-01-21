49de74c9b27df14afa569ab79e1c0a68b78f0222
Mark <mjagerton@ucdavis.edu>
2020-01-20 16:14:46 -0800

    stop converting kappa (oops)

--- example/run-estimator.jl
+++ example/run-estimator.jl
@@ -156,7 +156,7 @@ elseif length(THETA0_FULL_OVERRIDE) != 0
     @warn "Not using theta supplied - incorrect length"
 end
 
-if CONVERT_KAPPA
+if false # CONVERT_KAPPA
     let r = theta_royalty(dataset_full, theta0_full),
         kap = theta_royalty_κ(data_royalty, r)
         update_kappa_level_to_cumsum!(kap)
