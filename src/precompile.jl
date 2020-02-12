0503ee8dab9431ac5c91b3d8d779620fbe63ed46
Mark <mjagerton@ucdavis.edu>
2020-02-11 18:27:56 -0800

    shorten ShaleDrillingLikelihood.SDLParameters to SDL via constant

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,5 @@
+const SDL = ShaleDrillingLikelihood.SDLParameters
+
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
 
@@ -11,7 +13,8 @@ function _precompile_()
         DrillingCost_TimeFE,
         DrillingCost_TimeFE_rigrate,
         DrillingCost_TimeFE_costdiffs,
-        DrillingCost_TimeFE_rig_costdiffs
+        DrillingCost_TimeFE_rig_costdiffs,
+        DrillingCost_DoubleTimeFE,
     )
 
     for R in revs
@@ -21,15 +24,15 @@ function _precompile_()
             @assert precompile(Tuple{Type{DataProduce},        DR, String})
             @assert precompile(Tuple{Type{DataRoyalty},        DR, String})
             @assert precompile(Tuple{Type{DynamicDrillModel},  DR, Float64,LeasedProblem,Tuple{StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}},UnitRange{Float64}},SparseArrays.SparseMatrixCSC{Float64,Int64},StepRangeLen{Float64,Base.TwicePrecision{Float64},Base.TwicePrecision{Float64}},Bool})
-            @assert precompile(Tuple{Core.kwftype(typeof(ShaleDrillingLikelihood.SDLParameters.GridTransition)),NamedTuple{(:minp,),Tuple{Float64}},typeof(ShaleDrillingLikelihood.SDLParameters.GridTransition),DataDrillPrimitive{DR, ExogTimeVars{2,Tuple{Float64,Float64},StepRange{Dates.Date,Dates.Month}},Tuple{Float64,Float64},Int64,LeasedProblem},Float64,Int64})
+            @assert precompile(Tuple{Core.kwftype(typeof(SDL.GridTransition)),NamedTuple{(:minp,),Tuple{Float64}},typeof(SDL.GridTransition),DataDrillPrimitive{DR, ExogTimeVars{2,Tuple{Float64,Float64},StepRange{Dates.Date,Dates.Month}},Tuple{Float64,Float64},Int64,LeasedProblem},Float64,Int64})
         end
     end
 
-    @assert precompile(Tuple{typeof(ShaleDrillingLikelihood.SDLParameters.parse_commandline)})
-    @assert precompile(Tuple{typeof(ShaleDrillingLikelihood.SDLParameters.PsiSpace),Int64})
-    @assert precompile(Tuple{typeof(ShaleDrillingLikelihood.grad_simloglik!),SubArray{Float64,1,SharedArrays.SharedArray{Float64,2},Tuple{Array{Int64,1},Int64},  false}, ShaleDrillingLikelihood.ObservationGroup{DataProduce{ProductionModel,Float64},Int64},Array{Float64,1},SimulationDraws{Float64,1,SubArray{Float64,1,Array{Float64,2},Tuple{Base.Slice{Base.OneTo{Int64}},Int64},true}}})
-    @assert precompile(Tuple{typeof(ShaleDrillingLikelihood.simloglik!),     SubArray{Float64,1,SharedArrays.SharedArray{Float64,2},Tuple{UnitRange{Int64},Int64},true},  ShaleDrillingLikelihood.ObservationGroup{DataRoyalty{RoyaltyModel,Int64,Array{Float64,1},Float64},Int64},SubArray{Float64,1,Array{Float64,1},Tuple{UnitRange{Int64}},true},SimulationDraws{Float64,1,SubArray{Float64,1,Array{Float64,2},Tuple{Base.Slice{Base.OneTo{Int64}},Int64},true}},Bool})
+    @assert precompile(Tuple{typeof(SDL.parse_commandline)})
+    @assert precompile(Tuple{typeof(SDL.PsiSpace),Int64})
+    @assert precompile(Tuple{typeof(grad_simloglik!),SubArray{Float64,1,SharedArrays.SharedArray{Float64,2},Tuple{Array{Int64,1},Int64},  false}, ObservationGroup{DataProduce{ProductionModel,Float64},Int64},Array{Float64,1},SimulationDraws{Float64,1,SubArray{Float64,1,Array{Float64,2},Tuple{Base.Slice{Base.OneTo{Int64}},Int64},true}}})
+    @assert precompile(Tuple{typeof(simloglik!),     SubArray{Float64,1,SharedArrays.SharedArray{Float64,2},Tuple{UnitRange{Int64},Int64},true},  ObservationGroup{DataRoyalty{RoyaltyModel,Int64,Array{Float64,1},Float64},Int64},SubArray{Float64,1,Array{Float64,1},Tuple{UnitRange{Int64}},true},SimulationDraws{Float64,1,SubArray{Float64,1,Array{Float64,2},Tuple{Base.Slice{Base.OneTo{Int64}},Int64},true}},Bool})
     @assert precompile(Tuple{typeof(update_reo!),Array{Float64,1}})
-    @assert precompile(Tuple{Core.kwftype(typeof(ShaleDrillingLikelihood.start_up_workers)),NamedTuple{(:nprocs,),Tuple{Int64}},typeof(start_up_workers),Base.EnvDict})
+    @assert precompile(Tuple{Core.kwftype(typeof(start_up_workers)),NamedTuple{(:nprocs,),Tuple{Int64}},typeof(start_up_workers),Base.EnvDict})
 
 end
